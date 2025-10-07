cc52e4ecc2f5fed78bc50cedfac4fe81ab6ba3aa
praveendpjs <praveenaishu2012@gmail.com>
2025-10-07 09:58:46 +0530

    Create reverse_string.js

---
+++ reverse_string.js
@@ -0,0 +1,9 @@
+// reverse a string without using inbuilt functions
+function reverseString(str){
+    let reversed = '';
+    for(let i=str.length-1; i>= 0; i--){
+        reversed += str[i];
+    }
+    return reversed;
+}
+console.log(reverseString("hello"));
