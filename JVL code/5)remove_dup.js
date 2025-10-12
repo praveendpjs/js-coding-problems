8354d5ae60f82ffdefbe1f05c3b5aa73cc01c213
praveendpjs <praveenaishu2012@gmail.com>
2025-10-12 10:11:45 +0530

    Update 5)remove_dup.js

--- JVL code/5)remove_dup.js
+++ JVL code/5)remove_dup.js
@@ -6,8 +6,27 @@
 // }
 
 // Example2: Using filter and indexOf()
+// function removeDuplicate(arr){
+//     return arr.filter((val, index, self) => self.indexOf(val) === index)
+// }
+
+// Example3: Using reduce method
 function removeDuplicate(arr){
-    return arr.filter((val, index, self) => self.indexOf(val) === index)
+    return arr.reduce((unique, item) =>{
+        return unique.includes(item) ? unique: [...unique,item]
+    },[])
 }
+
+
+// Example4: Using for loop
+// function removeDuplicate(arr){
+//     let result = [];
+//     for(let i = 0; i < arr.length; i++){
+//         if(result.indexOf(arr[i]) === -1){
+//             result.push(arr[i])
+//         }
+//     }
+//     return result
+// }
 
 console.log(removeDuplicate([1,2,3,4,5,5,1,5,3]));
