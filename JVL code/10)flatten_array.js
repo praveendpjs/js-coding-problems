8f41c8d2a137bee5e50c53376120af98c0b8ab81
praveendpjs <praveenaishu2012@gmail.com>
2025-10-22 09:47:48 +0530

    changes

---
+++ JVL code/10)flatten_array.js
@@ -0,0 +1,8 @@
+const nestedArray = [1,2,[3,4,[5,6],7],8,9]
+function flattenarray(arr){
+    let flatArr = []
+    arr.forEach(item =>{
+
+    })
+
+}
--- JVL code/9)find_missing.js
+++ JVL code/9)find_missing.js
@@ -1,9 +1,21 @@
 // Find the missing number in an array of size n-1 containing numbers 1 to n
-function findMissing(arr){
-    const n = arr.length + 1
-    const totalSum = (n*(n+1))/2
-    const arrSum = arr.reduce((acc, cur) => acc + cur,0 )
+// Example 1
+// function findMissing(arr){
+//     const n = arr.length + 1
+//     const totalSum = (n*(n+1))/2
+//     const arrSum = arr.reduce((acc, cur) => acc + cur,0 )
 
-    return totalSum - arrSum
-} 
+//     return totalSum - arrSum
+// } 
+// console.log(findMissing([1,2,4,5]));
+
+// Example 2
+function findMissing(arr){
+    const newSet = new Set(arr)
+    for(let i = 1; i <= arr.length+1; i++){
+        if(!newSet.has(i)){
+            return i 
+        }
+    }
+}
 console.log(findMissing([1,2,4,5]));
