558e771339299b78f3059f89ccf6a316c74485c6
praveendpjs <praveenaishu2012@gmail.com>
2025-10-22 12:46:49 +0530

    Update 10)flatten_array.js

--- JVL code/10)flatten_array.js
+++ JVL code/10)flatten_array.js
@@ -2,7 +2,16 @@ const nestedArray = [1,2,[3,4,[5,6],7],8,9]
 function flattenarray(arr){
     let flatArr = []
     arr.forEach(item =>{
+        if(Array.isArray(item)){
+            flatArr = flatArr.concat(flattenarray(item))
+            
+        }else{
+            flatArr.push(item)
+        }
 
     })
+    return flatArr
 
 }
+console.log(flattenarray(nestedArray));
+
