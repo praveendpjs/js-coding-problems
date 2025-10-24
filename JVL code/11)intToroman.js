a04ab8736213af6db49469ef0c8c081be02e4ff9
praveendpjs <praveenaishu2012@gmail.com>
2025-10-24 14:11:37 +0530

    Update 11)intToroman.js

--- JVL code/11)intToroman.js
+++ JVL code/11)intToroman.js
@@ -1,6 +1,34 @@
-function inToRoman(num){
-    const roman = [
-        {value:1000, symbol:M},
-        {}
+function intToRoman(num) {
+    const romanMap = [
+        { value: 1000, symbol: 'M' },
+        { value: 900, symbol: 'CM' },
+        { value: 500, symbol: 'D' },
+        { value: 400, symbol: 'CD' },
+        { value: 100, symbol: 'C' },
+        { value: 90, symbol: 'XC' },
+        { value: 50, symbol: 'L' },
+        { value: 40, symbol: 'XL' },
+        { value: 10, symbol: 'X' },
+        { value: 9, symbol: 'IX' },
+        { value: 5, symbol: 'V' },
+        { value: 4, symbol: 'IV' },
+        { value: 1, symbol: 'I' },
     ]
+    let roman = '';
+    for (let i = 0; i < romanMap.length; i++) {
+        const { value, symbol } = romanMap[i]; //Destructuring (normally we would've written const value = romanMap[i].value; const symbol = romanMap[i].symbol;)
+        // It creates two new variables:
+
+        // value — holding the value from the object (like 1000)
+
+        // symbol — holding the symbol from the object (like 'M')
+
+        while (num >= value) {
+            roman += symbol;
+            num -= value;
+
+        }
+    }
+    return roman
 }
+console.log(intToRoman(8));
