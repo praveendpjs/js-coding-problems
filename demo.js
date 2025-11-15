bc9a2ba37f317f5f0c99db1d2929b714bd5e0d36
praveendpjs <praveenaishu2012@gmail.com>
2025-11-15 17:04:11 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,27 +1,5 @@
-function intToRoman(num) {
-    const romanMap = [
-        { value: 1000, symbol: 'M' },
-        { value: 900, symbol: 'CM' },
-        { value: 500, symbol: 'D' },
-        { value: 400, symbol: 'CD' },
-        { value: 100, symbol: 'C' },
-        { value: 90, symbol: 'XC' },
-        { value: 50, symbol: 'L' },
-        { value: 40, symbol: 'XL' },
-        { value: 10, symbol: 'X' },
-        { value: 9, symbol: 'IX' },
-        { value: 5, symbol: 'V' },
-        { value: 4, symbol: 'IV' },
-        { value: 1, symbol: 'I' },
-    ]
-    let roman = '';
-    for(const i of romanMap){
-        while (num >= i.value) {
-            roman += i.symbol;
-            num -= i.value;
-        }
-    }
-    
-    return roman
-}
-console.log(intToRoman(10));
+let i = 6;
+do {
+    console.log(i);
+    i++;
+} while (i <= 5);
